5792d132313316c2cd636483eb87f505ba200f09
Yegeurina <yegeurinasec@gmail.com>
2021-06-11 11:45:36 +0900

    1st Coding

---
+++ client.c
@@ -0,0 +1,372 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
+#include<pthread.h>
+#include<time.h>
+
+#define BUF_SIZE 1000
+#define NORMAL_SIZE 20
+
+void *send_msg(void *arg);
+void *recv_msg(void *arg);
+void error_handler(char *msg);
+
+void menu();
+void changeName();
+void menuOptions(int sock);
+void dutchPay(int sock);
+void miniGame(int sock);
+void fileTrasfer(int sock);
+void fileDownload(int sock);
+
+char filename[NORMAL_SIZE];
+char name[NORMAL_SIZE]="[DEFALT]";
+char msg_form[NORMAL_SIZE];
+char s_time[NORMAL_SIZE];
+char msg[BUF_SIZE];
+char s_port[NORMAL_SIZE];
+char c_ip[NORMAL_SIZE];
+
+volatile int flag=0;
+volatile int flagDetail=0;
+volatile int gameflag=0;
+
+pthread_mutex_t mutx;
+
+int main(int argc,char *argv[])
+{
+    int sock;
+    struct sockaddr_in s_adr;
+    pthread_t send_thread,recv_thread;
+    void* thread_return;
+
+    if(argv!=4)
+    {
+        printf(" Usage : %s <ip> <port> <name>\n", argv[0]);
+        exit(1);
+    }
+
+    /** local time **/
+    struct tm *t;
+    time_t timer = time(NULL);
+    t=localtime(&timer);
+    sprintf(serv_time, "%d-%d-%d %d:%d", t->tm_year+1900, t->tm_mon+1, t->tm_mday, t->tm_hour,t->tm_min);
+
+    sprintf(name, "  [%s] -", argv[3]);
+    sprintf(c_ip, "%s", argv[1]);
+    sprintf(s_port, "%s", argv[2]);
+
+    sock=socket(PF_INET, SOCK_STREAM,0);
+    memset(&s_adr,0,sizeof(s_adr));
+    s_adr.sin_family = AF_INET;
+    s_adr.sin_addr.s_addr=inet_addr(argv[1]);
+    s_addr.sin_port = htons(atoi(argv[2]));
+
+    if(connect(sock,(struct sockaddr_in*)&s_adr,sizeof(s_adr))==-1) error_handler("Connect Error");
+
+    menu();
+
+    pthread_create(&send_thread,NULL,send_msg,(void *)&sock);
+    pthread_create(&recv_thread,NULL,recv_msg,(void *)&sock);
+    pthread_join(send_thread,&thread_return);
+    pthread_join(recv_thread,&thread_return);
+
+    close(sock);
+
+    return 0;
+
+}
+
+void *send_msg(void *arg)
+{
+    int sock = *((int *)arg);
+    char name_msg[NORMAL_SIZE+BUF_SIZE];
+    char myInfo[BUF_SIZE];
+    char* who;
+    char temp[BUF_SIZE];
+
+
+    int k=0;
+
+    printf(">> Join the Chat\n");
+    sprintf(myInfo,"%s's join. IP : %s",name,c_ip);
+    write(sock,myInfo,strlen(myInfo));
+
+    while(1)
+    {
+        fgets(msg,BUF_SIZE,stdin);
+        if(!strcmp(msg,"!menu\n"))
+        {
+            menuOptions(sock);
+        }
+        else if(!strcmp(msg,"q\n") || !strcmp(msg,"Q\n"))
+        {
+            close(sock);
+            exit(0);
+        }
+
+        sprintf(name_msg,"%s %s",name,msg);
+        write(sock,name_msg,strlen(name_msg));
+    }
+
+    return NULL;
+
+}
+void *recv_msg(void *arg)
+{
+    int sock = *((int *)arg);
+    char name_msg[NORMAL_SIZE+BUF_SIZE];
+    int str_len;
+    char flag[NORMAL_SIZE];
+    char temp[NORMAL_SIZE];
+
+    int recvFlag =0;
+
+    while(1)
+    {
+        if(flag==2)
+        {
+            read(sock,flg,1);
+            recvFlag=atoi(flg);
+            if(recvFlag==1) printf("DOWN\n");
+            else if(recvFlag==2)    printf("UP\n");
+            else if(recvFlag==3)    printf("Error,Retry\n");
+            else if(!stnrcmp(flg,"miniGame",strlen("miniGame")))
+            {
+                printf("Congratulations! You are WINNER!\n");
+                memset(name_msg,0,sizeof(name_msg));
+                flag=0;
+            }
+        }
+        else if(flag==4 || flagDetail==9)
+        {
+            FILE *fp;
+            char filebuf[100];
+            memset(filebuf,0x00,100);
+            int read_cnt;
+            char fileSize[BUF_SIZE];
+            int ifSize=0;
+            strcpy(fileSize,name_msg);
+            fp=filename(filename,"wb");
+            ifSize = atoi(fileSize);
+            memset(name_msg,0,sizeof(name_msg));
+            usleep(4000000);
+            read_cnt = read(sock,filebuf,ifSize);
+            fwrite((void *)filebuf,1,read_cnt,fp);
+            printf("%s is stored!!",filename);
+            fclose(fp);
+            flag=0;
+            flagDetail=0;
+        }
+        str_len=read(sock,name_msg,NORMAL_SIZE+BUF_SIZE-1);
+        if(str_len==-1) return (void*)-1;
+        name_msg[str_len]=0;
+        fputs(name_msg,stdout);
+    }
+    return NULL;
+}
+void error_handler(char *msg)
+{
+    fputs(msg,stderr);
+    fputs("\n",stderr);
+    exit(1);
+}
+
+void menu()
+{
+    system("clear");
+    printf(" **** Chat Client ****\n");
+    printf(" server port : %s \n", s_port);
+    printf(" client IP   : %s \n", c_ip);
+    printf(" chat name   : %s \n", name);
+    printf(" server time : %s \n", s_time);
+    printf(" ************* menu ***************\n");
+    printf(" if you want to select menu -> !menu\n");
+    printf(" 1. change name\n");
+    printf(" 2. clear/update\n");
+    printf(" **********************************\n");
+    printf(" Exit -> q & Q\n\n");
+}
+void changeName()
+{
+    char nameTemp[100];
+    printf("Input new name : ");
+    scanf("%s",nameTemp);
+    strcpy(msg,name);
+    strcat(msg,"--> Change Name -->");
+    sprintf(name,"[%s]",nameTemp);
+    printf("\nComplete\n");
+}
+void menuOptions(int sock)
+{
+    int select;
+    printf("\n***** menu mode *****\n");
+    printf("1. change name\n");
+    printf("2. clear / update\n");
+    printf("3. dutchpay\n");
+    printf("4. minigame\n");
+    printf("5. file transfer\n");
+    printf("6. file download\n");
+    printf("the other key is cancel\n");
+    printf("********************\n");
+    printf(">> ");
+
+    scanf("%d",&select);
+    getchar();
+    switch(select)
+    {
+        case 1 :
+            changeName();
+            flag=0;
+            break;
+        case 2 :
+            menu();
+            flag=0;
+            break;
+        case 3 :
+            printf("dutchpay function start\n");
+            flag=1;
+            dutchPay(sock);
+            break;
+        case 4:
+            printf(/"minigame function start\n");
+            flag=2;
+            miniGame(sock);
+            break;
+        case 5 :
+            printf("filetransfer function start\n");
+            flag=3;
+            fileTrasfer(sock);
+            break;
+        case 6 : 
+            printf("filedownload function start\n");
+            flag=4;
+            fileDownload(sock);
+            flagDetail=9;
+            flag=0;
+            usleep(1000000);
+            break;
+        default :
+            printf("cancel.\n")
+            flag=0;
+            break;
+    }
+}
+void dutchPay(int sock)
+{
+    int price;
+    char totalPrice[100];
+    int howMany;
+    char howm[100];
+
+    strcpy(msg,"dutch"); // function flag
+    write(sock,msg,BUF_SIZE);
+    
+    printf("Input How many? ");
+    scanf("%d",&howMany);
+    sprintf(howm,"%d",howMany);
+    write(sock,howm,2); //People
+
+    printf("Input total price : ");
+    scanf("%d",&price);
+    sprintf(totalPrice,"%d",price);
+    write(sock,totalPrice,10); //Total Price
+
+    memset(msg,0,sizeof(msg));
+}
+void miniGame(int sock)
+{
+    int gameNum;
+    int gamec[100];
+    char under[]="Under\n";
+    char up[]="Up\n";
+    char plz[]="Please Input Number\n";
+
+    while(1)
+    {
+        strcpy(msg,"miniGame");
+        write(sock,msg,strlen(msg));
+        memset(msg,0,sizeof(msg));
+
+        printf("Guess Number!! Choose between 1~999(second line is fake) : \n");
+        scanf("%d",&gameNum);
+
+        sprintf(gamec,"%d",gameNum);
+        write(sock,gamec,4);
+    }
+    memset(msg,0,sizeof(msg));
+}
+void fileTrasfer(int sock)
+{
+    int i=0;
+    FILE *fp;
+    char fileBuf[BUF_SIZE];
+    int read_cnt;
+    char name_cnt[NORMAL_SIZE];
+    int ifSize=0;
+    char fSize[5];
+
+    char name_msg[NORMAL_SIZE+BUF_SIZE];
+    memset(name_msg,0,sizeof(name_msg));
+    strcpy(msg,"transfer");
+    write(sock,msg,strlen("transfer"));
+    
+    printf("Input filename : ");
+    fgets(filename,NORMAL_SIZE,stdin);
+
+    sprintf(name_cnt,"%d",strlen(filename));
+    write(sock,name_cnt,2);
+
+    wirte(sock,filename,strlen(filename));
+
+    fp=fopen(filename,"rb");
+    fseek(fp,0,SEEK_END);
+    ifSize = ftell(fp);
+    fseek(fp,0,SEEK_SET);
+    
+    sprint(fSize,"%d",ifSize);
+    write(sock,fSize,5);
+    
+    if(fp!=NULL)
+    {
+        while(1)
+        {
+            read_cnt = fread((void *)fileBuf,1,ifSize,fp);
+            if(read_cnt<ifSize)
+            {
+                write(sock,fileBuf,read_cnt);
+                break;
+            }
+            write(sock,fileBuf,read_cnt);
+        }
+        fclose(fp);
+    }
+}
+void fileDownload(int sock)
+{
+    char name_msg[NORMAL_SIZE+BUF_SIZE];
+    memset(name_msg,0,sizeof(name_msg));
+    
+    FILE *fp;
+    char filebuf[BUF_SIZE];
+    int read_cnt;
+    char name_cnt[2];
+    char filesize[5];
+    int ifSize=0;
+    char fSize[5];
+
+    strcpy(msg,"download");
+    write(sock,msg,strlen(msg));
+
+    printf("Input Filename : ");
+    fgets(filename,NORMAL_SIZE,stdin);
+    
+    sprintf(name_cnt,"%d",strlen(filename));
+    write(sock,name_cnt,2);
+
+    write(sock,filename,strlen(filename));
+
+}
---
+++ server.c
@@ -0,0 +1,274 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<pthread.h>
+#include<time.h>
+
+#define BUF_SIZE 1000
+#define MAX_CLIENT 10
+#define MAX_IP 30
+#define NAME_SIZE 20
+
+void error_handler(char*msg);
+void* client_handler(void *arg);
+void send_msg(char *msg,int len);
+void menu(char port[]);
+char* serverState(int count);
+
+char msgpay[BUF_SIZE];
+int flag=0;
+
+int client_cnt;
+int client_socket[MAX_CLIENT];
+pthread_mutex_t mutx;
+
+int main(int argc, char *argv[])
+{
+    int s_sock, c_sock;
+    struc sockaddr_in s_adr, c_adr;
+    int c_adr_size;
+    pthread_t t_id;
+
+    //set time log
+    sturct tm *t;
+    time_t timer = time(NULL);
+    t=localtime(&timer);
+
+    if(argc!=2) //port input error
+    {
+        printf(" Usage : %s <port>\n", argv[0]);
+        exit(1);
+    }
+    
+    //port information
+    menu(argv[1]);
+
+    pthread_mutex_init(&mutx,NULL);
+    s_sock = socket(PF_INET,SOCK_STREAM,0);
+    //server socket set : TCP, IPv4
+    memset(&s_adr,0,sizeof(s_adr));
+    s_adr.sin_family=AF_INET;
+    s_adr.sin_addr.s_addr=htonl(INADDR_ANY);
+    s_adr.sin_port = hotns(atoi(argv[1]));
+    
+    if(bind(s_sock, (struct sockaddr*)&s_adr,sizeof(s_adr)==-1))    error_handler("Bind Error");
+    if(listen(s_sock,5)==-1)    error_handler("Listen Error");
+
+    while(1)
+    {
+        t=localtime(&timer);
+        if(client_cnt<MAX_CLIENT)
+        {
+            c_adr_size = sizeof(c_adr);
+            c_sock=accept(s_sock,(struct sockaddr*)&c_adr,&c_adr_size);
+            
+            pthread_mutex_lock(&mutx);
+            client_socket[client_cnt++]=c_sock;
+            pthread_mutex_unlock(&mutx);
+
+            pthread_create(&t_id,NULL,client_handler,(void*)&c_sock); //thread start
+            pthread_detach(t_id);
+
+            printf("Connected Client IP : %s",inet_ntoa(c_adr.sin_adddr));
+            printf("(%4d-%02d-%02d %02d:%02d)\n",t.tm_year+1900,t.tm_mon+1,t.tm_mday,t.tm_hour,t.tm_min);
+            printf("User(%d/%d)\n",client_cnt,MAX_CLIENT);
+        }
+        else
+        {
+            printf("Too many User\n");
+        }
+    }
+    close(s_sock);
+    return 0;
+    
+
+}
+void* client_handler(void *arg)
+{
+    int c_sock = *((int *)arg);
+    int str_len =0 , i;
+    char msg[BUF_SIZE];
+
+    FILE *fp;
+    char filename[BUF_SIZE];
+    char filebuf[BUF_SIZE];
+
+    char name_cnt[2];
+
+    siae_t bufsize =0;
+    int nbyte;
+    memset(filebuf,0x00,30);
+    char filesize[5];
+
+    int read_cnt;
+
+    char totalprice[BUF_SIZE];
+    char howm[BUF_SIZE];
+    int price = 0;
+    int howMany= 0;
+    int result = 0;
+    char result_c[BUF_SIZE];
+
+    int gamenum;
+    char gamec[BUF_SIZE];
+    char temp[BUF_SIZE];
+
+    char flag[BUF_SIZE];
+    char gamemsg[BUF_SIZE];
+
+    int win, won;
+
+    char under[BUF_SIZE];
+    char up[BUF_SIZE];
+    char please[BUF_SIZE];
+
+    won = (rand()%1000)+1;
+    while(1)
+    {
+        read(c_sock,flag,BUF_SIZE);
+        if(!strncmp(flag,"dutch",strlen("dutch")))
+        {
+            read(c_sock,howm,2);
+            howMany = atoi(howm);
+
+            read(c_sock,totalprice,10);
+            price = atoi(totalprice);
+
+            strcpy(msg,"people : ");
+            strcat(msg,howm);
+            strcat(msg,", totalprice : ");
+            strcat(msg,totalprice);
+            strcat(msg," WON, per person : ");
+            result = price / howMany;
+            sprintf(result_c,"%d",result);
+            strcat(msg,result_c);
+            strcat(msg,result_c);
+            strcat(msg," WON");
+            str_len = strlen(msg);
+        }
+        else if(!strncmp(flag,"miniGame",strlen("miniGame")))
+        {
+            sprintf(under,"%d",1);
+            sprintf(up,"%d",2);
+            sprintf(please,"%d",3);
+            while(!strncmp(flag,"miniGame",strlen("miniGame")))
+            {
+                read(c_sock,gamec,4);
+                win = atioi(gamc);
+                if(won==win)
+                {
+                    write(c_sock,"miniGame",BUF_SIZE);
+                    break;
+                }
+                else if(win>won)    write(c_sock,under,2);
+                else if(win<won) wite(c_sock,up,2);
+                else write(c_sock,please,2);
+            }
+            memset(msg,0,sizeof(msg));
+        }
+        else if(!strncmp(flag,"transfer",strlen("transfer")))
+        {
+            memset(msg,0,sizeof(msg));
+            read(c_sock,name_cnt,2);
+            read(c_sock,filename,atoi(name_cnt));
+            fp=fopen(filename,"wb");
+            read(c_sock,filesize,5);
+
+            int fileSize = atoi(filesize);
+            read_cnt = read(c_sock,filebuf,fileSize);
+            
+            fwrite((void *)filebuf,1,read_cnt,fp);
+            
+            strcpy(msg,filesize);
+            strcpy(msg,filename);
+            strcat(msg,",stored.\n");
+            strcat(msg,filebuf);
+
+            str_len=strlen(msg);
+            fclose(fp);
+        }
+        else if(!strncmp(flag,"download",strlne("download")))
+        {
+            int ifsize =0;
+            char fsize[5];
+            memset(msg,0,sizeof(msg));
+            read(c_sock,name_cnt,2);
+            read_cnt(c_sock,filename,atoi(name_cnt));
+            
+            fp = fopen(filename,"rb");
+            
+            fseek(fp,0,SEEK_END);
+            ifsize=ftell(fp);
+            fseek(fp,0,SEEK_SET);
+
+            sprintf(fsize,"%d",ifsize);
+            write(c_sock,fsize,5);
+
+            if(fp!=NULL)    read_cnt = fread((void *)filebuf,1,ifsize,fp);
+
+            usleep(5000000);
+            strcpy(msg,filebuf);
+            fclose(fp);
+        }
+        else
+        {
+            str_len = read(c_sock,msg,sizeof(msg));
+            if(str_len==0) break;
+        }
+        
+        send_msg(msg,str_len);
+    }
+
+    //remove disconnected client
+    pthread_mutex_lock(&mutx);
+    for(i=0;i<client_cnt;i++)
+    {
+        if(c_sock==client_socket[i])
+        {
+            while(i++<client_cnt-1)
+                client_socket[i]=client_socket[i+1];
+            break;
+        }
+    }
+    client_cnt--;
+    pthread_mutex_unlock(&mutx);
+    close(c_sock);
+    return NULL;
+
+}
+void send_msg(char *msg,int len)
+{
+    int i;
+    pthread_mutex_lock(&mutx);
+    for(i=0;i<client_cnt;i++)   write(client_socket[i],msg,lent);
+    pthread_mutex_unlock(&mutx);
+}
+char* serverState(int count)
+{
+    char* stateMsg = malloc(sizeof(char)*NAME_SIZE);
+    strcpy(stateMsg,"NONE");
+    if(count<5) strcpy(stateMsg,"GOOD");
+    else    strspy(stateMsg,"BAD");
+    return stateMsg;
+}
+void menu(char port[])
+{
+    system("clear");
+
+    printf("***** chat server *****\n");
+    printf("server port : %s\n",port);
+    printf("server state : %s\n",serverState(client_cnt));
+    printf("max connection : %d\n",MAX_CLIENT);
+
+    printf("\n***** LOG *****\n");
+}
+void error_handler(char *msg)
+{
+    fputs(msg,stderr);
+    fputs("\n",stderr);
+    exit(1);
+}
